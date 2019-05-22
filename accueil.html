72a8215e552c14466a68f5c4eb452275588fc937
tb59 <thomas.bourdeaudhuy@gmail.com>
2019-05-22 09:35:26 +0200

    questions 1,2

---
+++ accueil.html
@@ -0,0 +1,3 @@
+<h1>Accueil</h1>
+
+<a href="redirection.php">Lien vers redirection.php</a>
---
+++ redirection.php
@@ -0,0 +1,13 @@
+<?php
+// Page redirection.php
+
+header("Location:accueil.html");
+// demande de redirection
+
+die("message"); // Interrompt l'interprétation du reste de la page
+
+?>
+
+<h1> Page de redirection </h1>
+
+Ce texte est envoyé au navigateur 
